9b4a339a13653823e415ee8765c2cbb5c6942095
Jashan Gill <jpsgill6@gmail.com>
2025-08-06 16:46:34 +0530

    fixes

--- client/src/app/api/snippets/route.ts
+++ client/src/app/api/snippets/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/auth'
 import connectDB from '@/lib/connectDB'
 import Snippet from '@/models/snippet'
 import { NextResponse } from 'next/server'
---
+++ client/src/lib/auth.ts
@@ -0,0 +1,15 @@
+
+import GitHubProvider from 'next-auth/providers/github'
+import { NextAuthOptions } from 'next-auth'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+}
+
+
