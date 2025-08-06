f4bfd1410c71f5b3d4ac741891e778e6a24d6116
Jashan Gill <jpsgill6@gmail.com>
2025-08-06 16:52:01 +0530

    fix

--- client/src/app/api/snippets/[id]/route.ts
+++ client/src/app/api/snippets/[id]/route.ts
@@ -1,15 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import connectDB from '@/lib/connectDB'
 import Snippet from '@/models/snippet'
-import { NextResponse } from 'next/server'
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
   const session = await getServerSession(authOptions)
-  const id1 = await params;
-  if (!session?.user) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+
+  if (!session?.user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
 
   await connectDB()
-  await Snippet.deleteOne({ _id: id1.id, userId: session.user.email })
+
+  const { id } = await context.params
+
+  await Snippet.deleteOne({ _id: id, userId: session.user.email })
+
   return NextResponse.json({ success: true })
 }
